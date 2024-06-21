2640dfa6a4fe221e851852fa5a15b1920c6b815a
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-21 11:12:47 +0300

    add console log

--- middleware/showInitDataMiddleware.js
+++ middleware/showInitDataMiddleware.js
@@ -1,9 +1,11 @@
 function getInitData(res) {
+  console.log('res: ', res);
   return res.locals.initData;
 }
 
 const showInitDataMiddleware = (_req, res, next) => {
   const initData = getInitData(res);
+  console.log('getInitData: ', getInitData);
   if (!initData) {
     return next(
       new Error('Cant display init data as long as it was not found'),
