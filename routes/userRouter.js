a4bd4998997de6730728e1ffc6b45b7c7759b2a4
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-24 14:19:51 +0300

    update signup user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -14,14 +14,14 @@ router.route('/signup').post(async (req, res) => {
     const { user } = res.locals.initData;
     console.log('user: ', user);
 
-    const [newUser, created] = User.findOrCreate({
+    const [newUser] = User.findOrCreate({
       where: { tgId: user.id },
       defaults: user,
     });
 
-    if (!created) {
-      return res.status(403).json({ message: 'User already exists' });
-    }
+    // if (!created) {
+    //   return res.status(403).json({ message: 'User already exists' });
+    // }
     return res.status(200).json(newUser);
   } catch (error) {
     console.log(error);
@@ -30,27 +30,3 @@ router.route('/signup').post(async (req, res) => {
 });
 
 module.exports = router;
-
-//
-// initData:  {
-//   authDate: 2024-06-24T08:13:11.000Z,
-//   hash: '6e78b4cb71e7bbff90901557c947ecb5db3fd2586d8e9f0c3b73263de5621e0d',
-//   queryId: 'AAH7r9MJAAAAAPuv0wlhYywV',
-//
-// {
-//     allowsWriteToPm: true,
-//     firstName: 'Masha',
-//     id: 164868091,
-//     languageCode: 'en',
-//     lastName: '',
-//     username: 'mashak000'
-//   }
-// }
-// user:  {
-//   allowsWriteToPm: true,
-//   firstName: 'Masha',
-//   id: 164868091,
-//   languageCode: 'en',
-//   lastName: '',
-//   username: 'mashak000'
-// }
