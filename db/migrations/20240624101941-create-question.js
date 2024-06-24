08271aa32d5d5ee7bab4960cc11f82ea733d8cd3
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-24 13:20:30 +0300

    create seeds

---
+++ db/migrations/20240624101941-create-question.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      question: {
+        type: Sequelize.TEXT
+      },
+      placeholder: {
+        type: Sequelize.STRING
+      },
+      theme: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  }
+};
