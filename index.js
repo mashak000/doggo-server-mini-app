fa7daee27531b54d2d3cea4bf35c9b302278ad81
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-25 14:40:19 +0300

    fix authMiddleware

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// app.use(authMiddleware);
+app.use(authMiddleware);
 // app.get('/', showInitDataMiddleware);
 app.use('/api/users', userRouter);
 app.use('/api/dogs', dogsRouter);
