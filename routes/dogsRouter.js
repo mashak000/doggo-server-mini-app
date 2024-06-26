4c423be8f41c81f0840a8205e1885173a07e4fec
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-26 09:08:06 +0300

    uload photo

--- routes/dogsRouter.js
+++ routes/dogsRouter.js
@@ -29,25 +29,25 @@ router
 
 router.route('/uploadImage').post(
   // uploadPhoto.array('data', 10),
-  // upload.single('photo'),
+  upload.single('photo'),
   async (req, res) => {
     console.log(req.body);
     console.log(req.file);
     console.log(req.files);
-    // try {
-    //   const uploadedFiles = [];
-    //   for (let i = 0; i < req.files.length; i += 1) {
-    //     const { path: oldPath, originalname } = req.files[i];
-    //     const parts = originalname.split('.');
-    //     const ext = parts[parts.length - 1];
-    //     const newPath = `${oldPath}.${ext}`;
-    //     fs.renameSync(oldPath, newPath);
-    //     uploadedFiles.push(newPath.replace('uploads/', ''));
-    //   }
-    //   res.json(uploadedFiles);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    try {
+      const uploadedFiles = [];
+      for (let i = 0; i < req.files.length; i += 1) {
+        const { path: oldPath, originalname } = req.files[i];
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = `${oldPath}.${ext}`;
+        fs.renameSync(oldPath, newPath);
+        uploadedFiles.push(newPath.replace('uploads/', ''));
+      }
+      res.json(uploadedFiles);
+    } catch (error) {
+      console.log(error);
+    }
   },
 );
 
