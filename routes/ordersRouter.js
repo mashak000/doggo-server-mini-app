78850d7dcc74febeb575ba3f54458b53e5fa4a1e
mashak000 <mashakgood@gmail.com>
2024-06-27 15:44:15 +0300

    history orders added

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -6,7 +6,21 @@ const router = Router();
 router.route('/').get(async (req, res) => {
   const { id } = res.locals.initData.user;
   const data = await Order.findAll({
-    where: { userId: id },
+    where: { userId: id, isActive: true },
+    include: [
+      { model: User },
+      { model: Dog },
+      { model: Sitter },
+      { model: Tariff },
+    ],
+  });
+  return res.json(data);
+});
+
+router.route('/history').get(async (req, res) => {
+  const { id } = res.locals.initData.user;
+  const data = await Order.findAll({
+    where: { userId: id, isActive: false },
     include: [
       { model: User },
       { model: Dog },
