4043e7c884654eae2d009a8118a79cc4486f9ce0
mashak000 <mashakgood@gmail.com>
2024-06-27 22:05:12 +0300

    sitter foto

--- db/seeders/20240624102005-Sitter.js
+++ db/seeders/20240624102005-Sitter.js
@@ -7,11 +7,11 @@ module.exports = {
         {
           firstName: 'Polina',
           lastName: '',
-          about:
-            "Your dog's new bestie, ready for all the walks and playtime!",
+          about: "Your dog's new bestie, ready for all the walks and playtime!",
           status: 'senior sitter',
           orderCounter: 28,
-          image: 'https://highlysensitiverefuge.com/wp-content/uploads/2022/10/why-highly-sensitive-people-have-a-special-connection-to-animals.jpg',
+          image:
+            'https://drive.google.com/file/d/1aq69FYu5BV5uLxxsyiVzhSn2o7UIJiq4/view?usp=drive_link',
         },
 
         {
@@ -21,7 +21,8 @@ module.exports = {
             'Here to make sure your dog has a tail-wagging great time with us!',
           status: 'handler',
           orderCounter: 36,
-          image: 'https://people.com/thmb/TqRgcEn65STk2WOu3f4ZWf5vBPU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(959x584:961x586)/dog-e10b5a9331054f2cafc93d2b6eec918a.jpg'
+          image:
+            'https://drive.google.com/file/d/1Tx92e2nebvYumLsY9aC72wFTdM6iQYRS/view?usp=drive_link',
         },
 
         {
@@ -31,7 +32,8 @@ module.exports = {
             "Your go-to for making sure your pup has the best adventures while you're away!",
           status: 'sitter',
           orderCounter: 12,
-          image: 'https://t3.ftcdn.net/jpg/01/80/85/16/360_F_180851624_5jvAslMGfdGeunZD92tglYgnJEnvdxOY.jpg',
+          image:
+            'https://drive.google.com/file/d/1Gh-sH6zUKiu5s434VJHMQddWsBz4PxbW/view?usp=drive_link',
         },
 
         {
@@ -41,7 +43,8 @@ module.exports = {
             'Here to give your pup all the love and attention they deserve!',
           status: 'sitter',
           orderCounter: 7,
-          image: 'https://media.istockphoto.com/id/1350689855/photo/portrait-of-an-asian-man-holding-a-young-dog.jpg?s=612x612&w=0&k=20&c=Iw0OedGHrDViIM_6MipHmPLlo83O59by-LGcsDPyzwU=',
+          image:
+            'https://drive.google.com/file/d/1TntTM61dPZXL9FqCnJgqj_FkDr3mh_zm/view?usp=drive_link',
         },
 
         {
@@ -51,6 +54,8 @@ module.exports = {
             "The energetic dog walker who's excited to meet your furry friend!",
           status: 'sitter',
           orderCounter: 2,
+          image:
+            'https://drive.google.com/file/d/1FiVRjX7_WRU4AXBiIiWhF6bZngCMgAEj/view?usp=drive_link',
         },
 
         // {
